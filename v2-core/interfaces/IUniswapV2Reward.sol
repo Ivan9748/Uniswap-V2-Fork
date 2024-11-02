9f225ab87606850da49adb1ab7023bd4584e4841
Ivan <89786209+Ivan9748@users.noreply.github.com>
2024-11-03 01:26:31 +0200

    Update IUniswapV2Reward.sol

--- v2-core/interfaces/IUniswapV2Reward.sol
+++ v2-core/interfaces/IUniswapV2Reward.sol
@@ -2,6 +2,12 @@ pragma solidity >=0.5.0;
 
 interface IUniswapV2Reward {
     function factory() external view returns(address);
+    function WETH() external view returns(address);
+    function owner() external view returns(address);
+    function myToken() external view returns(address);
+    function threshold() external view returns(uint);
+    function feeBalances(address _address) external view returns(uint);
+    function reserve() external view returns(uint);
 
     function check(address _token, uint _amount) external returns(bool result);
     function buyBack(address _token, uint _amount) external;
